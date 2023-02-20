6844df128efc027d8c27d4a15d510c5d6b3163d0
КарзиновВа <@211-10>
2023-02-20 18:14:19 +0300

    Добавьте файлы проекта.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,166 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using VIN_LIB;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Check_Contry_Asia() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "JNKBV61E57M729118";
+            string expected = "Азия";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Asia1() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "KMHDU4AD4AU955646";
+            string expected = "Азия";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Not_Exist() //не сущ
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "*06VR52R9WR220134";
+            string expected = "ошибка";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Amer() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "106VR52R9WR220134";
+            string expected = "Северная Америка";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Europa4() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "W06VR52R9WR220134";
+            string expected = "Европа";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_South_Amer() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "806VR52R9WR220134";
+            string expected = "Южная Америка";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Europa() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "W06VR52R9WR220134";
+            string expected = "Европа";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Contry_Africa() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "HH4KA855FMC0120GQ";
+            string expected = "Африка";
+
+            string actual = class1.GetVINCountry(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_VinWith_Wrong_VIS_Q() //неккоректооный vis
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "HH4KA855FMC0120GQ";
+            bool expected = false;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_VinWith_Wrong_VDS_Q() //неккоректооный vin
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "HH4KA8Q5FMC0120GG";
+            bool expected = false;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_VinWith_Wrong_WMS_q() //неккоректооный wms не сущ букв
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "QH4KA815FMC0120GG";
+            bool expected = false;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Check_Vin_With_Wrong_Vis() //неккоректооный vis
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "JH4KA815FMC0120GG";
+            bool expected = false;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Vin_normal() //простая проверка
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "JH4KA8GGFMC012098";
+            bool expected = true;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Check_Vin_With_WMS() //
+        {
+            Class1 class1 = new Class1();
+
+            string vin = "GH4KA8GGFMC012098";
+            bool expected = true;
+
+            bool actual = class1.CheckVIN(vin);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+}
---
+++ VIN_LIB/Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VIN_LIB
+{
+    public class Class1
+    {
+        public  bool CheckVIN(string vin)
+        {
+            char[] vinSymb =
+             {'0', '1', '2', '3', '4', '5', '6', '7', '8','9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z' };
+            char[] zapret = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'I', 'O', 'Q' };
+            char[] zapretAll = { 'I', 'O', 'Q' };
+            char[] num = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            if (vin.Length != 17)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < 17; i++)
+            {
+                foreach(char c in zapretAll)
+                {
+                    if (vin[i] == c)
+                        return false;
+                }
+            }
+            //првоерка на WMI
+            foreach (char c in zapret)
+            {
+                if (vin[0] == c)
+                    return false;
+                if (vin[1] == c)
+                    return false;
+               
+            }
+
+            
+
+            //vis послед цифры
+            
+                //for (int i = 12; i < 16; i++)
+                //{
+                //    foreach(char j in num)
+                //    {
+                //      if (vin[i] == j)
+                //        return false;
+                //     }
+                    
+                //}
+           
+
+            return true;
+        }
+       public  string GetVINCountry(string vin)
+         {
+            int ind = -1;
+            char[] vinSymb =
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8','9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H','J', 'K', 'L', 'M', 'N', 'P', 'R', 'S', 'T','U', 'V', 'W', 'X', 'Y', 'Z' };
+
+            for (int i = 0; i < vinSymb.Length; i++)
+            {
+                
+                if (vin[0] == vinSymb[i])
+                {
+                    ind = i;
+                }
+            }
+            if (ind >= 1 && ind <= 5)
+            {
+                return "Северная Америка";
+            }
+            if (ind >= 6 && ind <= 7)
+            {
+                return "Океания";
+            }
+            if (ind >= 8 && ind <= 9)
+            {
+                return "Южная Америка";
+            }
+            if (ind >= 10 && ind <= 17)
+            {
+                return "Африка";
+            }
+            if (ind >= 18 && ind <= 24)
+            {
+                return "Азия";
+            }
+            if (ind >= 25 && ind <= 29)
+            {
+                return "Европа";
+            }
+         
+            return "ошибка";
+        }
+    }
+
+}
+    
+
+
